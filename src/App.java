85baada5dff3779524f362b1120bba662ce6b891
Luis <105742218+AnibalRocha@users.noreply.github.com>
2023-01-08 19:02:59 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -6,5 +6,19 @@ public class App {
        // Deber Horario
     Horario e1= new Horario();
     e1.mostrar();
+    cuadrado();
+    }
+    
+    private static void cuadrado() {
+        int tanano= 5;
+
+        for (int f = 0; f < tanano; f++) {
+            for (int c = 0; c < tanano; c++)
+                if (f==0 || c==0 || f == tanano -1 || c==tanano -1 )
+                    System.out.print(((c+f)%2==0)?"+ ":"- "); 
+                else
+                    System.out.print("  "); 
+            System.out.println(" "); 
+        }
     }
 }
